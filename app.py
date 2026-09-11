d89da5cfd4a838db3b0fedfea6b047c6c8ea2b32
juan david <sacul010393@gmail.com>
2026-09-11 00:04:21 -0400

    reemplazar add_rows por line_chart para compatibilidad

--- app.py
+++ app.py
@@ -13,13 +13,15 @@ def toss_coin(n):
     mean = None
     outcome_no = 0
     outcome_1_count = 0
+    means = []
 
     for r in trial_outcomes:
         outcome_no += 1
         if r == 1:
             outcome_1_count += 1
         mean = outcome_1_count / outcome_no
-        chart.add_rows([mean])
+        means.append(mean)
+        chart.line_chart(mean)
         time.sleep(0.05)
 
     return mean
