11198d7205357660f15fb5793a0f8f0a32c6ed03
juan david <sacul010393@gmail.com>
2026-09-10 23:36:37 -0400

    initial commit

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ def toss_coin(n):
     return mean
 
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
-start_button = st.button('ejecutar')
+start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    mean = toss_coin(number_of_trials)
