270a5180b78c722f4a7410bf0a9f2b767225c069
juan david <sacul010393@gmail.com>
2026-09-11 19:53:50 -0400

    version 1

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ start_button = st.button('Run')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
-    st.session_state['experimento_no'] += 1
+    st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
     st.session_state['df_experiment_results'] = pd.concat([st.session_state['df_experimnent_results'], pd.DataFrame(data=[[st.session_state['experiment_no'], number_of_trials, mean]], columns=['no', 'iteraciones', 'media'])], axis=0)
     st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
