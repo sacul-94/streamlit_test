0fc33cd0ba329d23b693f47f9b18e22bfd0a4208
juan david <sacul010393@gmail.com>
2026-09-11 20:07:54 -0400

    version 1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import time
 
 # estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
 if 'experiment_no' not in st.session_state:
-    st.session_state['experimnent_no'] = 0
+    st.session_state['experiment_no'] = 0
 
 if 'df_experiment_resuts' not in st.session_state:
     st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
